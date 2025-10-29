243e904ba7b281923a08ee4d71946e4e47c4d091
Ian Manuel <153279114+IanPaniagua@users.noreply.github.com>
2025-10-29 19:05:51 +0100

    fix: add webpack config to ignore test-firebase

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,24 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  
+  // Exclude test pages from build
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+    }
+    
+    // Ignore test-firebase directory
+    config.module = config.module || {};
+    config.module.rules = config.module.rules || [];
+    config.module.rules.push({
+      test: /src\/app\/test-firebase/,
+      loader: 'ignore-loader',
+    });
+    
+    return config;
+  },
 };
 
 export default nextConfig;
